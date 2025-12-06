8d9ed0742914787bfcdc7dad0855746bf90d5290
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2025-12-06 16:42:47 +0100

    chore:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,48 @@
-import { Image } from "@/components";
+import { Image, SectionTitle } from "@/components";
 
 export default function Home() {
   return (
-    <div id="banner" className="container-fluid py-5 h-100">
-      <div className="container px-1 px-lg-0 h-100">
-        <div className="row align-items-center align-items-center h-100">
-          <div className="col-lg-9">
-            <div className="d-flex flex-column gap-5 mb-4">
-              <div className="flex-shrink-0">
-                <h1 className="display-1 regular mb-0">
-                  Security Engineer building <br className="d-none d-lg-block" />
-                  resilient systems at scale, turning complexity into clarity through
-                  automation that's intelligent, <br className="d-none d-lg-block" /> purposeful, and effective.
-                </h1>
-              </div>
-              <div className="flex-shrink-0 mb-5">
-                <div className="d-flex flex-row align-items-center gap-3 text-muted mb-5">
-                  <small className="d-flex align-items-center">
-                    Working out of
-                    <span className="d-flex align-items-center fw-medium text-dark ms-1">
-                      <Image src="/assets/icons/map.svg" alt="map" width={14} height={14} className="me-1" />
-                      Remote
-                    </span>
-                  </small>
-                  <small className="d-flex align-items-center">
-                    Local Time
-                    <span className="d-flex align-items-center fw-medium text-dark ms-1">
-                      <Image src="/assets/icons/clock.svg" alt="clock" width={14} height={14} className="me-1" />
-                      4:07 PM
-                    </span>
-                  </small>
+    <>
+      <div id="banner" className="container-fluid py-5 h-100">
+        <div className="container px-1 px-lg-0 h-100">
+          <div className="row align-items-center align-items-center h-100">
+            <div className="col-lg-9">
+              <div className="d-flex flex-column gap-5 mb-4">
+                <div className="flex-shrink-0">
+                  <h1 className="display-1 regular mb-0">
+                    Security Engineer building <br className="d-none d-lg-block" />
+                    resilient systems at scale, turning complexity into clarity through
+                    automation that's intelligent, <br className="d-none d-lg-block" /> purposeful, and effective.
+                  </h1>
+                </div>
+                <div className="flex-shrink-0 mb-5">
+                  <div className="d-flex flex-row align-items-center gap-3 text-muted mb-5">
+                    <small className="d-flex align-items-center">
+                      Working out of
+                      <span className="d-flex align-items-center fw-medium text-dark ms-1">
+                        <Image src="/assets/icons/map.svg" alt="map" width={14} height={14} className="me-1" />
+                        Remote
+                      </span>
+                    </small>
+                    <small className="d-flex align-items-center">
+                      Local Time
+                      <span className="d-flex align-items-center fw-medium text-dark ms-1">
+                        <Image src="/assets/icons/clock.svg" alt="clock" width={14} height={14} className="me-1" />
+                        4:07 PM
+                      </span>
+                    </small>
 
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <SectionTitle title="Featured Work" />
+
+      <SectionTitle title="Contact" />
+    </>
   );
 }
--- src/components/ui/ui.component.tsx
+++ src/components/ui/ui.component.tsx
@@ -1,5 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
+import { SectionTitleProps } from "@/interfaces";
+
+export function SectionTitle({ title }: SectionTitleProps) {
+    return (
+        <div className="container-fluid border-bottom h-89">
+            <div className="container px-1 px-lg-0">
+                <span className="text-uppercase fw-light">{title}</span>
+            </div>
+        </div>
+    )
+}
 
 export {
     Link,
