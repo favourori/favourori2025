b46a4f93044a4363a000a5ab4ad9ec997c34928e
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2025-12-06 17:35:37 +0100

    chore:

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -6,10 +6,10 @@ export function GuestHeader() {
         <header className="container-fluid border-bottom d-flex align-items-center fixed-top h-84" id="header">
             <nav className="container container-lg px-1 px-lg-0">
                 <div className="row justify-content-between align-items-center">
-                    <div className="col-auto">
+                    <Link href="/" className="col-auto">
                         <span className="d-block regular fs-5 lh-28">Favour <span className="regular-italic">Ori</span></span>
                         <span className="d-block smaller text-muted lh-16">Security Engineer</span>
-                    </div>
+                    </Link>
                     <div className="col-auto">
                         <div className="d-flex gap-4">
                             {GuestHeaderMenu.map((m) => (
