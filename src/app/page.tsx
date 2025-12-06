2afa4fb4e0d27f61c51d2815a8ef05597c7f1500
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2025-12-06 17:49:25 +0100

    chore:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,15 @@ export default function Home() {
                 <div className="flex-shrink-0">
                   <div className="d-flex flex-row align-items-center gap-3 text-muted">
                     <small className="d-flex align-items-center">
-                      Working out of
-                      <span className="d-flex align-items-center fw-medium text-dark ms-1">
+                      <span className="opacity-50">Working out of</span>
+                      <span className="d-flex align-items-center fw-medium text-dark ms-2">
                         <Image src="/assets/icons/map.svg" alt="map" width={14} height={14} className="me-1" />
                         Remote
                       </span>
                     </small>
                     <small className="d-flex align-items-center">
-                      Local Time
-                      <span className="d-flex align-items-center fw-medium text-dark ms-1">
+                      <span className="opacity-50">Local Time</span>
+                      <span className="d-flex align-items-center fw-medium text-dark ms-2">
                         <Image src="/assets/icons/clock.svg" alt="clock" width={14} height={14} className="me-1" />
                         4:07 PM
                       </span>
