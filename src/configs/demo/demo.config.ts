8ffa6d888b8591f3cdfea5565323cb1e7dc8f4be
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2025-12-13 13:52:16 +0100

    chore:

--- src/configs/demo/demo.config.ts
+++ src/configs/demo/demo.config.ts
@@ -177,9 +177,9 @@ const works = [
 ];
 
 const galleryImages = [
-    ["1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg"],
-    ["1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg"],
-    ["1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg", "1.jpg", "2.jpg"],
+    ["6.jpg", "29.jpg", "9.jpg", "4.jpg", "28.jpg", "10.jpg", "7.jpg", "30.jpg", "5.jpg", "8.jpg"],
+    ["14.jpg", "3.jpg", "19.jpg", "12.jpg", "1.jpg", "15.jpg", "20.jpg", "2.jpg", "13.jpg", "1.jpg"],
+    ["27.jpg", "22.jpg", "16.jpg", "25.jpg", "18.jpg", "23.jpg", "21.jpg", "26.jpg", "24.jpg", "17.jpg"],
 ];
 
 export {
